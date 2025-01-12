9f2339e330dfdf5f35b536e3de82ed8c65601d80
DJ824 <106926891+DJ824@users.noreply.github.com>
2025-01-12 17:25:23 -0500

    Delete include/aws_config.h

--- include/aws_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-struct AWSConfig {
-    static constexpr char BUCKET_NAME[] = "hft-backtest-data";
-    static constexpr char REGION[] = "us-east-1";
-    static constexpr char CACHE_DIR[] = "./cache";
-};
-
